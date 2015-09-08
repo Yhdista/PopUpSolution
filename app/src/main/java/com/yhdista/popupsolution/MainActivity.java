abed04fa1f427647356339d458a444e0b7cbb226
Jan Vlach <jan.vlach@zentity.com>
2015-09-08 11:43:41 +0200

    Initial commit

---
+++ app/src/main/java/com/yhdista/popupsolution/MainActivity.java
@@ -0,0 +1,48 @@
+package com.yhdista.popupsolution;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView atv = (AutoCompleteTextView) findViewById(R.id.autotv);
+
+        String[] items = new String[]{"123", "124", "125", "126", "127","127545454"};
+        atv.setAdapter(new ArrayAdapter<String>(this, R.layout.drop_down_item, items));
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
